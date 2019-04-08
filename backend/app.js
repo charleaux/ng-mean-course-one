3e30cd625cf1c769978188a76a570a3dfc746477
Charles Torry <ctorry@gmail.com>
2019-04-08 00:03:54 -0400

    added routing as well as editing existing posts.

--- backend/app.js
+++ backend/app.js
@@ -29,7 +29,7 @@ app.use((req, res, next) => {
   );
   res.setHeader(
     'Access-Control-Allow-Methods',
-    'GET, POST, PATCH, DELETE, OPTIONS'
+    'GET, POST, PATCH, DELETE, OPTIONS, PUT'
   );
   next();
 });
@@ -40,12 +40,30 @@ app.post('/api/posts', (req, res, next) => {
     content: req.body.content
   })
   // req.body;
-  post.save();
-
-  res.status(201).json({
-    message: `Post added successfully: ${post}`
+  post.save().then(createdPost => {
+    res.status(201).json({
+      message: `Post added successfully: ${post}`,
+      postId: createdPost._id
+    });
   });
 });
+
+app.put('/api/posts/:id', (req, res, next) => {
+  const post = new Post({
+    _id: req.params.id,
+    title: req.body.title,
+    content: req.body.content
+  })
+  Post.updateOne({
+    _id: req.params.id
+  }, post).then((result) => {
+    console.log(result);
+    res.status(200).json({
+      message: "Update Successful!"
+    })
+  })
+
+})
 
 app.get('/api/posts', (req, res, next) => {
   Post.find().then(posts => {
@@ -54,7 +72,33 @@ app.get('/api/posts', (req, res, next) => {
       posts
     });
   });
+});
 
+app.get('/api/posts/:id', (req, res, next) => {
+  Post.findById(req.params.id).then(post => {
+    if (post !== undefined) {
+      res.status(200).json({
+        message: 'Post fetched succesfully!',
+        post
+      });
+    } else {
+      res.status(404).json({
+        message: 'Posts not found!'
+      });
+    }
+
+  });
+});
+
+app.delete('/api/posts/:id', (req, res, next) => {
+  Post.deleteOne({
+    _id: req.params.id
+  }).then((result) => {
+    console.log(result);
+    res.status(200).json({
+      message: "Post deleted!"
+    });
+  });
 });
 
 module.exports = app;
--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
 import { PostsService } from '../posts.service';
+import { ActivatedRoute, ParamMap } from '@angular/router';
+import { Post } from '../post.model';
 
 @Component({
   selector: 'app-post-create',
@@ -11,19 +13,57 @@ import { PostsService } from '../posts.service';
 export class PostCreateComponent implements OnInit {
   enteredTitle: string;
   enteredContent: string;
+  post: Post;
+  private mode: string;
+  private postId: string;
 
-  constructor(private postsService: PostsService) {}
+  constructor(
+    private postsService: PostsService,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit() {
     this.enteredTitle = '';
     this.enteredContent = '';
+    this.mode = 'create';
+    this.route.paramMap.subscribe((paramMap: ParamMap) => {
+      if (paramMap.has('postId')) {
+        this.mode = 'edit';
+        this.postId = paramMap.get('postId');
+        this.postsService
+          .getPost(this.postId)
+          .subscribe(
+            (postData: {
+              message: string;
+              post: { _id: string; title: string; content: string };
+            }) => {
+              this.post = {
+                id: postData.post._id,
+                title: postData.post.title,
+                content: postData.post.content
+              };
+            }
+          );
+      } else {
+        this.mode = 'create';
+        this.postId = undefined;
+      }
+    });
   }
 
-  onAddPost(form: NgForm) {
+  onSavePost(form: NgForm) {
     if (!form.valid) {
       return undefined;
     }
-    this.postsService.addPost(form.value.title, form.value.content);
+    if (this.mode === 'create') {
+      this.postsService.addPost(form.value.title, form.value.content);
+    } else {
+      this.postsService.updatePost(
+        this.postId,
+        form.value.title,
+        form.value.content
+      );
+    }
     form.resetForm();
   }
 }
